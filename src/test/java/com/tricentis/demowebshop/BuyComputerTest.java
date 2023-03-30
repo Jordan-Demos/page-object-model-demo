53505fd1a1d871e69483f38d401f39e1fa1b20e0
Jordan J. Carrillo Solis <jordan.jcarrillo.s@gmail.com>
2023-03-30 17:52:49 -0600

    set-up demo project

---
+++ src/test/java/com/tricentis/demowebshop/BuyComputerTest.java
@@ -0,0 +1,119 @@
+package com.tricentis.demowebshop;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.MyPage;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Random;
+
+public class BuyComputerTest {
+
+    @Test
+    public void addSimpleComputerToCart() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/web-drivers/" + "chromedriver.exe");
+
+//        ChromeOptions chromeOptions = new ChromeOptions();
+//        chromeOptions.addArguments("");
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://demowebshop.tricentis.com/");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(90));
+
+        WebElement registerButton = driver.findElement(By.className("ico-register"));
+        wait.until(ExpectedConditions.visibilityOf(registerButton));
+        wait.until(ExpectedConditions.elementToBeClickable(registerButton));
+        registerButton.click();
+
+        List<WebElement> genders = driver.findElements(By.xpath("//*[@class='gender']"));
+        wait.until(ExpectedConditions.visibilityOfAllElements(genders));
+
+        int randomGender = (int) Math.round(Math.random());
+        genders.get(randomGender).click();
+
+        Faker faker = new Faker();
+        String firstname = faker.name().firstName();
+        String lastname = faker.name().lastName();
+        String email = String.format("%s.%s@test.com", firstname, lastname).toLowerCase();
+        String randomPassword = faker.internet().password();
+
+        WebElement firstNameInput = driver.findElement(By.id("FirstName"));
+        firstNameInput.clear();
+        firstNameInput.sendKeys(firstname);
+
+        WebElement lastNameInput = driver.findElement(By.id("LastName"));
+        lastNameInput.clear();
+        lastNameInput.sendKeys(lastname);
+
+        WebElement emailInput = driver.findElement(By.id("Email"));
+        emailInput.clear();
+        emailInput.sendKeys(email);
+
+        WebElement passwordInput = driver.findElement(By.id("Password"));
+        passwordInput.clear();
+        passwordInput.sendKeys(randomPassword);
+        WebElement confirmPasswordInput = driver.findElement(By.id("ConfirmPassword"));
+        confirmPasswordInput.clear();
+        confirmPasswordInput.sendKeys(randomPassword);
+
+        WebElement confirmRegisterButton = driver.findElement(By.id("register-button"));
+        confirmRegisterButton.click();
+
+        WebElement successRegisterDiv = driver.findElement(By.xpath("//*[@class='page registration-result-page']"));
+        try {
+            wait.until(ExpectedConditions.visibilityOf(successRegisterDiv));
+        } catch (TimeoutException timeoutException) {
+            throw new RuntimeException(timeoutException.getMessage());
+        }
+
+        Actions actions = new Actions(driver);
+        WebElement computersHeader = driver.findElement(By.xpath("//*[@class='top-menu']//*[contains(@href,'computers')]"));
+        actions.moveToElement(computersHeader).build().perform();
+
+        WebElement deskComputers = computersHeader.findElement(By.xpath("./following-sibling::ul//*[contains(@href,'desktops')]"));
+        deskComputers.click();
+
+        WebElement simpleComputer = driver.findElement(By.xpath("//*[@data-productid='75']//*[@class='product-title']"));
+        simpleComputer.click();
+
+        List<WebElement> mandatoryFields = driver.findElements(By.xpath("//*[@class='attributes']//*[contains(@class,'required')]/parent::dt"));
+        for (WebElement mandatoryField : mandatoryFields) {
+            Random random = new Random();
+            List<WebElement> options = mandatoryField.findElements(By.xpath("./following-sibling::dd[1]//input"));
+            int selectOption = (int) Math.round(Math.random() * options.size());
+            selectOption = Math.min(selectOption, options.size() - 1);
+            options.get(selectOption).click();
+            System.out.println(String.format("Selecting option: %s: %s",
+                    (mandatoryField.findElement(By.xpath("./label"))).getText(),
+                    (options.get(selectOption).findElement(By.xpath("./following-sibling::label"))).getText()
+            ));
+        }
+
+        WebElement addToCartButton = driver.findElement(By.id("add-to-cart-button-75"));
+        addToCartButton.click();
+
+        WebElement loadingElement = driver.findElement(By.xpath("//*[@class='ajax-loading-block-window']"));
+        try {
+         loadingElement.isDisplayed();
+         wait.until(ExpectedConditions.invisibilityOf(loadingElement));
+        } catch (NoSuchElementException noSuchElementException) {
+            // Do nothing
+        }
+
+        WebElement barNotification = driver.findElement(By.id("bar-notification"));
+        Assert.assertTrue(barNotification.getAttribute("class").contains("success"), "Computer was not added to cart");
+
+        MyPage myPage = new MyPage(driver);
+        myPage.clickOnStart();
+    }
+}
